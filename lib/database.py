ba1fc14fb38952fc70752b41e427b7287ce676a3
RONNY <mboyaronny41@gmail.com>
2025-12-02 15:18:14 +0300

    database.py

---
+++ lib/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+DATABASE_URL = "sqlite://fitness_tracker.db"
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+sessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Database initialized successfully")
