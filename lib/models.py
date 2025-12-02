fc74382a67e1bb404a0f4dd5f39659c07bd35c0a
RONNY <mboyaronny41@gmail.com>
2025-12-02 15:39:45 +0300

    models.py

--- lib/models.py
+++ lib/models.py
@@ -2,3 +2,14 @@ from sqlalchemy import Column, Integer, String, Float, DateTime, Date, Boolean, 
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from lib.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable = False)
+    age = Column(Integer)
+    weight = Column(Float)
+    fitness_goal = Column(String)
+    created_at = Column(DateTime, default=datetime.now )
