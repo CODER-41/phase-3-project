1b1aa522072937c6d8eebba62d5e71ee0ad99e13
RONNY <mboyaronny41@gmail.com>
2025-12-02 15:32:30 +0300

    models.py

---
+++ lib/models.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, Date, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from lib.database import Base
